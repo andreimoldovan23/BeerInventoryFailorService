c613d5da3ff5bfa5a5b0aec2923d13fa9a1e92d7
andreimoldovan23 <moldovanandrei2301@gmail.com>
2021-07-21 21:59:24 +0300

    Enriched data model

--- src/main/java/sfmc/inventoryfailover/web/model/BeerInventoryDTO.java
+++ src/main/java/sfmc/inventoryfailover/web/model/BeerInventoryDTO.java
@@ -1,6 +1,7 @@
 package sfmc.inventoryfailover.web.model;
 
 import java.io.Serializable;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
@@ -13,6 +14,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class BeerInventoryDTO implements Serializable {
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+
+    @Builder.Default
+    private OffsetDateTime createdDate = OffsetDateTime.now();
+
+    @Builder.Default
+    private OffsetDateTime lastModifiedDate = OffsetDateTime.now();
+
+    @Builder.Default
+    private String upc = "123456";
+
     @Builder.Default
     private UUID beerId = UUID.fromString("00000000-0000-0000-0000-000000000000");
 
