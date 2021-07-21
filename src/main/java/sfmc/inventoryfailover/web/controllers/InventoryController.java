005965e5d609f033da56455df357b29504ed49ad
andreimoldovan23 <moldovanandrei2301@gmail.com>
2021-07-21 15:05:10 +0300

    Refactored

--- src/main/java/sfmc/inventoryfailover/web/controllers/InventoryController.java
+++ src/main/java/sfmc/inventoryfailover/web/controllers/InventoryController.java
@@ -1,17 +1,15 @@
 package sfmc.inventoryfailover.web.controllers;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import sfmc.inventoryfailover.web.model.BeerInventoryDTO;
 
 @RestController
 public class InventoryController {
-    @GetMapping("/api/v1/beer/{beerId}/inventory")
-    public List<BeerInventoryDTO> getInventoryByBeerId(@PathVariable UUID beerId) {
-        return List.of(BeerInventoryDTO.builder().beerId(beerId).build());
+    @GetMapping("/api/v1/inventory-failover")
+    public List<BeerInventoryDTO> getInventoryByBeerId() {
+        return List.of(BeerInventoryDTO.builder().build());
     }
 }
